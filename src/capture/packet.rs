8573fc12c528fdedea623ad6346a60e50d359412
Mandroso22 <goliatherz@gmail.com>
2025-07-09 21:25:07 -0400

    packet.rs

--- src/capture/packet.rs
+++ src/capture/packet.rs
@@ -6,3 +6,31 @@ pub struct Packet {
     pub ip: Option<IpPacket>,
     pub transport: Option<TransportPacket>,
 }
+
+/// Network header
+
+pub struct EthernetHeader {
+    pub dst_mac: MacAddr,
+    pub src_mac: MacAddr,
+    pub ether_type: EtherType,
+}
+
+pub struct IpHeader {
+    pub version: u8,
+    pub src_ip: IpAddr,
+    pub dist_ip: IpAddr,
+    pub protocol: u8,
+}
+
+/// Packets parsing 
+
+pub fn parse_ethernet_frame(data: &[u8]) -> Result<EthernetHeader, ParseError>
+pub fn parse_ip_packet(data: &[u8]) -> Result<IpHeader, ParseError>
+
+/// type error
+
+pub enum ParseError {
+    InvalidLength,
+    UnsupportedProtocol,
+    ChecksumMismatch,
+}
